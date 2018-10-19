523c828c2b53d96fbc3ab7f46407f21733d37342
Joan Jara <joan.jara@estudiants.urv.cat>
2018-10-20 01:05:39 +0200

    We implemented the method MinMax but lacks 3 more methods (heuristic, finalNode and nextNode)

--- ai_tictacgo.go
+++ ai_tictacgo.go
@@ -1,8 +1,22 @@
 package main
 
+import "math"
+
+const level_Max int = 9
+
 type Ai struct {
 	FinishGame bool
 	ToeBoard   [][]string
+}
+
+type Node struct {
+	node      Ai
+	heuristic int
+}
+
+func (e Node) Node(nodeConstructor Ai, value int) {
+	e.heuristic = value
+	e.node = nodeConstructor
 }
 
 func (e Ai) Ai(finish bool, board [][]string) {
@@ -10,10 +24,53 @@ func (e Ai) Ai(finish bool, board [][]string) {
 	e.ToeBoard = board
 }
 
-func (e Ai) MixMax() {
-
+func heuristica(e Ai, level int) int {
+	return 0
 }
 
-func heuristica(ToeBoard [][]string) int {
-	return 0
+func finalNode(nodeToAvaluate Node) bool {
+	return false
+}
+
+func nextNode(nodeToAvaluate Node) Node {
+	return nodeToAvaluate
+}
+
+func (e Ai) MinMax(alpha int, beta int, node Node, level int) Node {
+	var result Node
+	var value int
+	var next Node
+	var aux Node
+
+	if finalNode(node) {
+		if (level % 2) == 0 { //machine
+			result.Node(e, math.MinInt64)
+			return result
+		} else { //player
+			result.Node(e, math.MaxUint64)
+			return result
+		}
+	} else if level == level_Max {
+		value = heuristica(e, level)
+		result.Node(e, value)
+		return result
+	} else {
+		for alpha < beta {
+			next = nextNode(node)
+			aux = e.MinMax(alpha, beta, next, level+1)
+			if (level % 2) == 0 {
+				if result.heuristic > alpha {
+					alpha = result.heuristic
+					result.node = aux.node
+				}
+			} else {
+				if result.heuristic < beta {
+					beta = result.heuristic
+					result.node = aux.node
+				}
+			}
+		}
+
+		return result
+	}
 }
