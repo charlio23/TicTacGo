da9b76dc3ad80cb510f82caa031f5837df864a02
Joan Jara <joan.jara@estudiants.urv.cat>
2018-10-20 15:55:16 +0200

    Changes in general

--- ai_tictacgo.go
+++ ai_tictacgo.go
@@ -1,48 +1,114 @@
 package main
 
-import (
-	"math"
-	"reflect"
-)
+import "strconv"
 
-const level_Max int = 9
+var origBoard = {0,1,2,3,4,5,6,7,8}
 
-type Ai struct {
-	FinishGame bool
-	ToeBoard   [][]string
+// human
+var huPlayer = "O";
+
+// ai
+var aiPlayer = "X";
+
+func win(board []string, player string) bool {
+
+	if (board[0] == player && board[1] == player && board[2] == player) ||
+		(board[3] == player && board[4] == player && board[5] == player) ||
+		(board[6] == player && board[7] == player && board[8] == player) ||
+		(board[0] == player && board[3] == player && board[6] == player) ||
+		(board[1] == player && board[4] == player && board[7] == player) ||
+		(board[2] == player && board[5] == player && board[8] == player) ||
+		(board[0] == player && board[4] == player && board[8] == player) ||
+		(board[2] == player && board[4] == player && board[6] == player) {
+		return true
+	} else {
+		return false
+	}
 }
 
-type Node struct {
-	node      Ai
-	heuristic int
+func filter(board []string, aux [] string, value int) [] string{
+	if board==nil{
+		return aux
+	}else{
+		i := append(aux,board[value])
+		filter(board,i,value-1)
+	}
+	return nil
 }
 
-func (e Node) Node(nodeConstructor Ai, value int) {
-	e.heuristic = value
-	e.node = nodeConstructor
+func choose(ss []string, test func(string) bool) (ret []string) {
+	for _, s := range ss {
+		if test(s) {
+			ret = append(ret, s)
+		}
+	}
+	return
 }
 
-func (e Ai) Ai(finish bool, board [][]string) {
-	e.FinishGame = finish
-	e.ToeBoard = board
+func emptyIndexies(board []string) []string{
+	var aux []string
+	return filter(board, aux, len(board))
 }
 
-func heuristica(e Ai, level int) int {
-	return 0
+func heuristica(board []string, availSpots []string)int{
+	if (win(board, huPlayer)){
+		return -10
+	} else if (win(board, aiPlayer)){
+		return 10
+	} else if (len(availSpots) == 0){
+		return 0
+	}
 }
 
-func finalNode(nodeToAvaluate Node) bool {
-	return reflect.DeepEqual(Node{}, nodeToAvaluate)
+type Move struct{
+	score int
+	index string
 }
 
-func nextNode(nodeToAvaluate Node) Node {
-	return nodeToAvaluate
+func movement (availSpots []string, player string, newBoard []string){
+	var moves = {}
+	var i int
+	i=0
+	// loop through available spots
+	for (i < len(availSpots)){
+		//create an object for each and store the index of that spot
+		var move Move
+		var j int = i
+
+		move.index = newBoard[strconv.Atoi(availSpots.[i])]
+
+		// set the empty spot to the current player
+		newBoard[availSpots[i]] = player;
+
+		/*collect the score resulted from calling minimax
+		  on the opponent of the current player*/
+		if (player == aiPlayer){
+			var result = MinMax(newBoard, huPlayer);
+			move.score = result.score;
+		}
+		else{
+			var result = MinMax(newBoard, aiPlayer);
+			move.score = result.score;
+		}
+
+		// reset the spot to empty
+		newBoard[availSpots[i]] = move.index;
+
+		// push the object to the array
+		append(move,moves);
+	i++;
+	}
 }
 
-func (e Ai) MinMax(alpha int, beta int, node Node, level int) Node {
-	var result Node
+func MinMax(board []string, player string) Move{
+	var availSpots = emptyIndexies(board)
+
+	var heur = heuristica(board,availSpots)
+
+	movement(availSpots,player,board)
+	/*var result Node
 	var value int
-	var next Node
+	var next []string
 	var aux Node
 
 	if finalNode(node) {
@@ -58,9 +124,10 @@ func (e Ai) MinMax(alpha int, beta int, node Node, level int) Node {
 		result.Node(e, value)
 		return result
 	} else {
-		for alpha < beta {
-			next = nextNode(node)
-			aux = e.MinMax(alpha, beta, next, level+1)
+		i:= len(e.ToeBoard)
+		for alpha < beta && i>0 {
+			next = emptyIndexies(e)
+			aux = next.node.MinMax(alpha, beta, next, level+1)
 			if (level % 2) == 0 {
 				if result.heuristic > alpha {
 					alpha = result.heuristic
@@ -74,6 +141,9 @@ func (e Ai) MinMax(alpha int, beta int, node Node, level int) Node {
 			}
 		}
 
-		return result
-	}
-}
+		return result*/
+
+
+		return 0
+		}
+
