2a721c1c9a85c2fcb34559b854ba814be86bd1f9
charlio23 <carlesbalsellsrodas@hotmail.com>
2018-10-20 20:22:10 +0200

    restapi implemented

--- main.go
+++ main.go
@@ -2,57 +2,46 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Val struct {
 	Value string `json:"value,omitempty"`
 }
 
-var Vals []Val
-
 func GetVals(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(Vals)
-}
-
-func encodeBoard(board [9]string) string {
-	value := ""
-	for i := 0; i < 9; i++ {
-		value += board[i]
-	}
-	return value
+	json.NewEncoder(w).Encode(r.Body)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+
 	params := mux.Vars(r)
-	for _, item := range Vals {
-		if item.Value == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
+	var board [9]string
+	var tab string = params["value"]
+	if len(tab) != 9 {
+		var val Val
+		val.Value = "error"
+		json.NewEncoder(w).Encode(&val)
+		return
+	} else {
+		for i := 0; i < len(tab); i++ {
+			board[i] = string(tab[i])
 		}
+		index, _ := strconv.Atoi(callAi(board))
+		board[index] = "X"
+		json.NewEncoder(w).Encode(&board)
 	}
-	json.NewEncoder(w).Encode(&Val{})
 }
 
 func main() {
 	//port := os.Getenv("PORT")
 	router := mux.NewRouter()
-	router.HandleFunc("/nextMove", index).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8000", router))
-	var urls = []string{
-		"http://google.com",
-		"http://facebook.com",
-		"http://youtube.com",
-		"http://yahoo.com",
-		"http://twitter.com",
-		"http://live.com",
-	}
-
-	urlsJson, _ := json.Marshal(urls)
-	fmt.Println(string(urlsJson))
+	router.HandleFunc("/nextMove/{value}", index).Methods("GET")
+	log.Fatal(http.ListenAndServe(":2000", handlers.CORS()(router)))
 	//var origBoard [9]string = [9]string{"0", "1", "2", "3", "4", "5", "6", "7", "8"}
 	//fmt.Println(callAi(origBoard))
 }
